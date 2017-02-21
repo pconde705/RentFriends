756c5742e5b8d999dc7420ef3e04e02903ee7dc5
Rebekka Rove <rebekka.rove@gmail.com>
2017-02-21 17:49:26 +0100

    added some css in index and show of offers

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20170221132519) do
     t.string   "photo"
   end
 
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.integer  "age"
