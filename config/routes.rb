3bb84521463dc2acbd4b4687fe7f3e7e4dc19a19
Rebekka Rove <rebekka.rove@gmail.com>
2017-02-21 09:53:38 +0100

    new navbar

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   get 'users/destroy'
 
   devise_for :users
-  root to: 'pages#home'
+  root to: 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,11 @@ ActiveRecord::Schema.define(version: 20170220153639) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.integer  "age"
