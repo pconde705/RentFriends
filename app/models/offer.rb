530da55857994a7ca7912b8a6e698d2e6c1bdb44
Patrick Conde <patrick.conde.costas@gmail.com>
2017-02-21 19:03:43 +0100

    Started Search, Not Finished

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,6 @@
 class Offer < ApplicationRecord
     has_attachment :photo
-    has_many :matches
+    has_many :matches, dependent: :destroy
 
   validates :city, presence: true, allow_blank: false
   validates :price, inclusion: { in: 0..10000, message: "No more than 10000" }, presence: true, allow_blank: false
