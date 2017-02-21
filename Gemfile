f2e59d3a9ece4f50af4261774276bcb9dbbb0472
Patrick Conde <patrick.conde.costas@gmail.com>
2017-02-21 14:19:49 +0100

    Added controller functionality (no design) to offers

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'figaro'
 gem 'jbuilder', '~> 2.0'
 gem 'redis'
 gem 'devise'
+gem 'cloudinary'
+
 
 gem 'sass-rails'
 gem 'jquery-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220153639) do
+ActiveRecord::Schema.define(version: 20170221102926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "pages", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "offers", force: :cascade do |t|
+    t.integer  "price"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
