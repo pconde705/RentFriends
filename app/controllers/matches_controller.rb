0be193a3350a7dc177ced7241801ec31c181b038
Patrick Conde <patrick.conde.costas@gmail.com>
2017-02-21 17:49:33 +0100

    Added Matches Model and Controller functionality

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,27 @@
+class MatchesController < ApplicationController
+  def new
+    @offer = Offer.find(params[:offer_id])
+    @match = Match.new
+  end
+
+  def create
+    @match = Match.new(match_params)
+    @match.offer = Offer.find(params[:offer_id])
+    @match.save
+
+    redirect_to offer_path(@match.offer)
+  end
+
+  def destroy
+    @match = Match.find(params[:id])
+    @match.destroy
+
+    redirect_to offer_path(@match.offer)
+  end
+
+  private
+
+  def match_params
+    params.require(:match).permit(:start_date, :end_date, :status)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221132519) do
+ActiveRecord::Schema.define(version: 20170221154004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,12 +30,25 @@ ActiveRecord::Schema.define(version: 20170221132519) do
     t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent", using: :btree
   end
 
+  create_table "matches", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "offer_id"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offer_id"], name: "index_matches_on_offer_id", using: :btree
+    t.index ["user_id"], name: "index_matches_on_user_id", using: :btree
+  end
+
   create_table "offers", force: :cascade do |t|
     t.integer  "price"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "photo"
+    t.string   "city"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,4 +71,6 @@ ActiveRecord::Schema.define(version: 20170221132519) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "matches", "offers"
+  add_foreign_key "matches", "users"
 end
