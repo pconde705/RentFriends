ea9062d6985b65ad42efcf38358d6612ff6734d7
Rebekka Rove <rebekka.rove@gmail.com>
2017-02-22 16:00:03 +0100

    added some css to show

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,11 @@ ActiveRecord::Schema.define(version: 20170222131534) do
     t.string   "gender"
   end
 
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.integer  "age"
